d4ffffad971b7618693e2f914bc0844d4a3ea650
廖宇凡 <xul4m3z06@gmail.com>
2018-09-06 12:49:59 +0800

    查詢特定時間＆即時更新 完成

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,16 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  res.render('index', { title: '林俊安' });
+  res.render('index', { title: '' });
 });
+
+router.get('/searchbyidtime', function(req, res, next) {
+
+    console.log(req.query)
+    res.render('searchbyidtime', { id: req.query.id,
+        timeinterval: req.query.timeinterval });
+});
+
 
 
 module.exports = router;
